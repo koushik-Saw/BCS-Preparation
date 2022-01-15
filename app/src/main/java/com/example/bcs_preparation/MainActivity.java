94757e4b5a738bbba0cf73b639d5824c4243d18d
Koushik Majumder <koushik15-7242@diu.edu.bd>
2022-01-15 11:52:32 +0600

    Project Created

---
+++ app/src/main/java/com/example/bcs_preparation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bcs_preparation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
