757f20117dbfa92c5fa78df9542173fa8a709dad
Koushik Majumder <koushik15-7242@diu.edu.bd>
2022-01-15 12:18:17 +0600

    initial stucture

--- app/src/main/java/com/example/bcs_preparation/view/MainActivity.java
+++ app/src/main/java/com/example/bcs_preparation/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.bcs_preparation;
+package com.example.bcs_preparation.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.bcs_preparation.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/bcs_preparation/view/authentication/Singup.java
@@ -0,0 +1,16 @@
+package com.example.bcs_preparation.view.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bcs_preparation.R;
+
+public class Singup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+    }
+}
